10b1e193ab60e24450c9aebcb12ba4ab9776976e
TengjunZhao <172549118@qq.com>
2023-09-06 17:13:21 +0800

    Develop
    1. Add start_time_in_prime.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import openpyxl
+import pymysql
+from datetime import datetime, timedelta
+
 
 def adjust_hitems_download(file_path:str):
     # 选择要操作的工作表（sheet）
@@ -21,6 +24,28 @@ def adjust_hitems_download(file_path:str):
     workbook.close()
 
 
+# 确定prime Yiled et搜索起始使时间
+def start_time_in_prime(table:str, field:str):
+    db_config = {
+        'host': 'localhost',
+        'user': 'remoteuser',
+        'password': 'password',
+        'database': 'cmsalpha'
+    }
+    # 建立数据库连接
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+    sql = f'SELECT MAX({field}) from {table};'
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    date_obj = datetime.strptime(res[0], "%Y-%m-%d")
+    new_date = date_obj + timedelta(days=1)
+    new_date_string = new_date.strftime("%Y-%m-%d")
+    return new_date_string
+
 if __name__ == "__main__":
-    adjust_hitems_download(file_path=r'D:\Python\RPA Common\ztj.xlsx')
+    # adjust_hitems_download(file_path=r'D:\Python\RPA Common\ztj.xlsx')
+    print(start_time_in_prime('db_primeyieldet', 'date_val'))
 
